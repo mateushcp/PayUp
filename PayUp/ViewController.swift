fd0f759d253e1cce26c2cf38a91a62935581a09c
Mateus Coelho <teus.hcp@gmail.com>
2025-04-21 18:00:21 -0300

    Initial Commit

---
+++ PayUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PayUp
+//
+//  Created by Mateus Henrique Coelho de Paulo on 21/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
